eb3d4f5551fdaa350ed36c3e82777ed12590c34b
Frank <codingfrank73@gmail.com>
2022-02-08 16:50:01 +0100

    rbg-wert angepasst

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 function changeBgColor() {
 
-    const x = ('rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')');
+    const x = ('rgb(' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ')');
 
     document.getElementById("bg-main").style.backgroundColor = x;
 
